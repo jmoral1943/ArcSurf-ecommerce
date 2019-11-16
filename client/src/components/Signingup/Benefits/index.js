fb3dd6ae99928209611dcc785d16e46f468f720e
Jonathan Morales <johnathanmorales@adminisorsmbp67.attlocal.net>
2019-11-16 14:48:08 -0500

    Privacy Policy and Terms of use completed

--- client/src/components/Signingup/Benefits/index.js
+++ client/src/components/Signingup/Benefits/index.js
@@ -6,7 +6,7 @@ const Benefits = () => {
       {/* <img src={require("../../../assets/png/joinImage.jpeg")} alt="Background with water"/> */}
       <img src={require("../../../assets/png/joinImage2.jpeg")} alt="Background with water" className="c-benefits__image"/>
       <div className="c-benefits-text">
-        <p className="c-benefits__title">Besides free shipping get rewards, faster checkout, and 200 points just for signing up DOPE.</p>
+        <p className="c-benefits__title">Besides free shipping get rewards, faster checkout, and 200 points just for signing up.</p>
         <p className="c-benefits__title">You already get your first reward.</p>
         <p className="c-benefits__title c-benefits__title--smaller">$1 spent = 1 point</p>
         <p className="c-benefits__title c-benefits__title--smaller">200 points = $15 off</p>
