f082bf7cf4b8ac947c00d387d33eb8bf8ec460da
Jonathan Morales <johnathanmorales@Administrators-MacBook-Pro-67.local>
2019-11-21 09:26:54 -0500

    Update server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 // middleware for adding secruity to the express application by providing secure headers
 app.use(helmet());
